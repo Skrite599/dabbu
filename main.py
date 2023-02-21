ef96c4549502b8ee17d2e250aa9b9765ff85d59f
Anirudh Pillutla <anirudhpillutla@Anirudhs-MacBook-Air-2.local>
2023-02-20 20:29:52 -0500

    add runnable

---
+++ main.py
@@ -0,0 +1,8 @@
+from service import service
+
+
+servicer = service.service(True, 1)
+
+
+if __name__ == "__main__":
+    servicer.whats_wrong()
---
+++ run.sh
@@ -0,0 +1,9 @@
+rm -rf venv
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+python setup.py
+
+python main.py
